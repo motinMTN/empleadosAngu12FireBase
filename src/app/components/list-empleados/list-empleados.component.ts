1a6eb4ed80b9fceb0056b674595f0f9a5c1c866b
motinMTN <luigi.motin@gmail.com>
2022-10-07 12:16:31 -0500

    Added image modification and deletion in Firebase when records are updated

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 import { Empleado } from 'src/app/interfaces/empleado';
 import { EmpleadoService } from 'src/app/services/empleado.service';
 import Swal from 'sweetalert2';
@@ -9,9 +10,11 @@ import Swal from 'sweetalert2';
   templateUrl: './list-empleados.component.html',
   styleUrls: ['./list-empleados.component.css']
 })
-export class ListEmpleadosComponent implements OnInit {
+export class ListEmpleadosComponent implements OnInit, OnDestroy {
 
   empleados: Empleado[] = [];
+  loading: boolean = false;
+  listObservers!: Array<Subscription>;
 
   constructor(private _empleadoService: EmpleadoService,
               private toastr: ToastrService) { }
@@ -21,23 +24,28 @@ export class ListEmpleadosComponent implements OnInit {
   }
 
   obtenerEmpleados(){
-    this._empleadoService.obtenerEmpleados().subscribe(data => {
+    this.loading = true;
+    const loadEmpleados = this._empleadoService.obtenerEmpleados().subscribe(data => {
       this.empleados = data;
       console.log(this.empleados);
+      this.loading = false;
     });
+    this.listObservers = [loadEmpleados];
   }
 
-  eliminarEmpleado(id: string){
+  eliminarEmpleado(empleado: Empleado){
     Swal.fire({
-      title: '¿Estás seguro de eliminar al empleado?',
+      title: `¿Estás seguro de eliminar al empleado: ${empleado.nombre}?`,
       text: 'No podrás recuperar esta información',
       icon: 'warning',
+      heightAuto: false,
       showCancelButton: true,
       confirmButtonText: 'Si',
       cancelButtonText: 'No'
     }).then((result) => {
+      this.loading = true;
       if (result.value) {
-        this._empleadoService.eliminarEmpleado(id).then( () => {
+        this._empleadoService.eliminarEmpleado(empleado).then( () => {
           this.toastr.info('El empleado fue eliminado con exito!', 'Empleado Eliminado',{
             positionClass: 'toast-bottom-right',
           });
@@ -45,7 +53,13 @@ export class ListEmpleadosComponent implements OnInit {
           this.toastr.error('No se ha podido eliminar el empleado! '+error, 'Empleado no eliminado');
         })
       }
-    })
+      this.loading = false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    console.log(`%c********** ngOnDestroy`, `color:blue`);
+    this.listObservers.forEach(sub => sub.unsubscribe());
   }
 
 }
